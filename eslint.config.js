6f62aea5fc23d35af01fede28d14b0f7a9be267a
Pavel Pogorelov <engoneer.pavel@yandex.ru>
2026-03-23 21:14:34 +0300

    линтеры преттиры

---
+++ eslint.config.js
@@ -0,0 +1,139 @@
+const eslint = require('@eslint/js');
+const { defineConfig } = require('eslint/config');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const unusedImports = require('eslint-plugin-unused-imports');
+const simpleImportSort = require('eslint-plugin-simple-import-sort');
+const prettier = require('eslint-plugin-prettier');
+
+module.exports = defineConfig([
+  {
+    ignores: ['projects/**'],
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+
+    plugins: {
+      'unused-imports': unusedImports,
+      'simple-import-sort': simpleImportSort,
+      '@typescript-eslint': tseslint.plugin,
+    },
+
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname,
+      },
+    },
+
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+
+      'unused-imports/no-unused-imports': 'error',
+
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          fixStyle: 'separate-type-imports',
+        },
+      ],
+
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/ban-types': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/explicit-function-return-type': ['error'],
+
+      '@angular-eslint/prefer-standalone': 'error',
+      '@angular-eslint/no-conflicting-lifecycle': 'error',
+      '@typescript-eslint/member-ordering': [
+        'error',
+        {
+          default: {
+            memberTypes: [
+              // поля
+              'private-static-field',
+              'private-field',
+              'decorated-field',
+              'protected-static-field',
+              'protected-field',
+              'public-static-field',
+              'public-field',
+
+              // конструктор
+              'constructor',
+
+              'public-static-method',
+              'public-method',
+              'protected-static-method',
+              'protected-method',
+              'private-static-method',
+              'private-method',
+            ],
+            order: 'as-written',
+          },
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          parser: 'angular',
+        },
+      ],
+      '@angular-eslint/template/click-events-have-key-events': 'off',
+      '@angular-eslint/template/interactive-supports-focus': 'off',
+      '@angular-eslint/template/no-autofocus': 'off',
+      '@angular-eslint/template/label-has-associated-control': 'off',
+      '@angular-eslint/template/elements-content': 'off',
+    },
+  },
+]);
